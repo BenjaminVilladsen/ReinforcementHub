c1b62736db9c562ca5b56b7839b8e7a2d2b13b7d
mua105310 <92716696+BenjaminVilladsen@users.noreply.github.com>
2024-03-11 13:29:41 +0100

    only up

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-"pip install swig"
-"pip install gymnasium[all]"
+#pip install swig
+#pip install gymnasium[all]
 
 import gymnasium as gym
 env = gym.make("LunarLander-v2", render_mode="human")
 observation, info = env.reset(seed=42)
 for _ in range(1000):
-   action = env.action_space.sample()  # this is where you would insert your policy
+   #action = env.action_space.sample()  # this is where you would insert your policy
+   action = 2
    observation, reward, terminated, truncated, info = env.step(action)
 
    if terminated or truncated:
