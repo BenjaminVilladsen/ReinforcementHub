8832237b6bc8ba97e765957b64b162bcc8b69bc9
Benjamin Michael Villadsen <benmicvil@gmail.com>
2024-04-05 11:23:53 +0200

    yes

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import pickle
+import time
+
 import gymnasium as gym
 import numpy as np
 from collections import deque
@@ -14,7 +17,7 @@ epsilon_decay = 0.99
 epsilon_min = 0.01
 epsilon = epsilon_start
 
-n_episodes = 6000
+n_episodes = 1000
 
 # Environment setup
 env = gym.make("LunarLander-v2",
@@ -24,6 +27,43 @@ nA = env.action_space.n
 
 # Initialize Q-table with a discrete approximation of the state space
 Q = np.zeros((n_bins,) * 8 + (nA,))
+
+#store policy and hyperparameters in a file
+def storePolicyAndHyperparamsInFile(fileName, Q, hyperparameters):
+    data = {
+        'Q_table': Q,
+        'hyperparameters': hyperparameters
+    }
+    with open(fileName, 'wb') as file:
+        pickle.dump(data, file)
+
+def loadPolicyAndHyperparamsFromFile(fileName):
+    """
+    Load the policy (Q-table) and hyperparameters from a specified file.
+
+    Parameters:
+    - fileName (str): The name of the file from which to load the data.
+
+    Returns:
+    - dict: A dictionary containing the Q-table under the key 'Q_table' and
+            the hyperparameters under the key 'hyperparameters'.
+    """
+    with open(fileName, 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+# Example usage
+
+
+shouldLoadFromFile = input("Do you want to load the policy and hyperparameters? (yes / just press enter) ")
+
+if shouldLoadFromFile.lower() == "yes":
+    loadFileName = input("Paste the file name here ")
+
+    loaded = loadPolicyAndHyperparamsFromFile(loadFileName)
+    hyperparameters = loaded['hyperparameters']
+    Q = loaded['Q_table']
+
 
 
 # Discretization function
@@ -73,50 +113,75 @@ episode_lengths = []
 
 
 # n step sarsas
-for i_episode in range(n_episodes):  # Number of episodes
-    epsilon = max(epsilon * epsilon_decay, epsilon_min)
-    #episode stats
-    total_reward = 0
-    steps = 0
+best_reward = -99999
 
-    observation, info = env.reset()
-    state = discretize(observation, n_bins, state_bounds)
-    action = epsilon_greedy_policy(state, Q, epsilon)
-    state_action_reward = deque(maxlen=n + 1)
+if (shouldLoadFromFile != "yes"):
+    for i_episode in range(n_episodes):  # Number of episodes
+        epsilon = max(epsilon * epsilon_decay, epsilon_min)
+        #episode stats
+        total_reward = 0
+        steps = 0
 
-    action_avg_reward = 0
+        observation, info = env.reset()
+        state = discretize(observation, n_bins, state_bounds)
+        action = epsilon_greedy_policy(state, Q, epsilon)
+        state_action_reward = deque(maxlen=n + 1)
 
-    while True:
-        observation, reward, done, truncated, info = env.step(action)
-        total_reward += reward
-        steps += 1
+        action_avg_reward = 0
 
-        next_state = discretize(observation, n_bins, state_bounds)
-        next_action = epsilon_greedy_policy(next_state, Q, epsilon)
-        state_action_reward.append((state, action, reward))
+        while True:
+            observation, reward, done, truncated, info = env.step(action)
+            total_reward += reward
+            steps += 1
 
-        if len(state_action_reward) >= n + 1:
-            state_to_update, action_to_update, _ = state_action_reward[0]
-            G = sum([gamma ** i * r for i, (_, _, r) in enumerate(state_action_reward)])
-            if not done and not truncated:
-                G += gamma ** n * Q[next_state][next_action]
-            Q = update_Q(Q, state_to_update, action_to_update, G, next_state, next_action, alpha, gamma)
+            next_state = discretize(observation, n_bins, state_bounds)
+            next_action = epsilon_greedy_policy(next_state, Q, epsilon)
+            state_action_reward.append((state, action, reward))
 
-        if done or truncated:
-            break
-        state = next_state
-        action = next_action
+            if len(state_action_reward) >= n + 1:
+                state_to_update, action_to_update, _ = state_action_reward[0]
+                G = sum([gamma ** i * r for i, (_, _, r) in enumerate(state_action_reward)])
+                if not done and not truncated:
+                    G += gamma ** n * Q[next_state][next_action]
+                Q = update_Q(Q, state_to_update, action_to_update, G, next_state, next_action, alpha, gamma)
 
-    episode_rewards.append(total_reward)
-    episode_lengths.append(steps)
-    #print stats
-    if (i_episode + 1) % (n_episodes // 10) == 0:
-        print(f"Episode: {i_episode + 1}, "
-              f"Average Reward: {np.mean(episode_rewards[-(n_episodes // 10):]):.2f}, "
-              f"Average Length: {np.mean(episode_lengths[-(n_episodes // 10):]):.2f}, "
-              f"Total Reward: {sum(episode_rewards[-(n_episodes // 10):]):.2f}")
+            if done or truncated:
+                break
+            state = next_state
+            action = next_action
 
-env.close()
+        episode_rewards.append(total_reward)
+        episode_lengths.append(steps)
+        if np.mean(episode_rewards[-(n_episodes // 10):]) > best_reward:
+            best_reward = np.mean(episode_rewards[-(n_episodes // 10):])
+
+        #print stats
+        if (i_episode + 1) % (n_episodes // 10) == 0:
+            print(f"Episode: {i_episode + 1}, "
+                  f"Average Reward: {np.mean(episode_rewards[-(n_episodes // 10):]):.2f}, "
+                  f"Average Length: {np.mean(episode_lengths[-(n_episodes // 10):]):.2f}, "
+                  f"Total Reward: {sum(episode_rewards[-(n_episodes // 10):]):.2f}")
+
+    env.close()
+
+#file name with best cost in policy and time and date
+#current time
+currentTimeStr = time.strftime("%Y%m%d-%H")
+fileName = f"best_reward:{best_reward}_time:{currentTimeStr}.txt"
+
+#as dict
+hyperparameters = {
+    'alpha': alpha,
+    'gamma': gamma,
+    'n': n,
+    'n_bins': n_bins,
+    'epsilon_start': epsilon_start,
+    'epsilon_decay': epsilon_decay,
+    'epsilon_min': epsilon_min,
+    'n_episodes': n_episodes
+}
+
+storePolicyAndHyperparamsInFile(fileName, Q, hyperparameters)
 
 
 # ---------------------------------------------------
@@ -142,3 +207,6 @@ for i_simulation in range(100):
             break
 
 env.close()
+
+
+
