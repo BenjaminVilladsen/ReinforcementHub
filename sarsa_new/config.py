21f839067e33f541db01a55d9ab6c0ca094e94a0
Benjamin Michael Villadsen <benmicvil@gmail.com>
2024-05-09 08:53:25 +0200

    Create config.py

---
+++ sarsa_new/config.py
@@ -0,0 +1,14 @@
+
+# LunarLander_statebounds
+state_bounds_lander = [(-1.5, 1.5), (-1.5, 1.5), (-5, 5), (-5, 5), (-3.1415927, 3.1415927), (-5, 5)]
+
+settings_lander = {
+    "alpha": 0.1,
+    "gamma": 0.9,
+    "epsilon": 0.1,
+    "num_bins": 20,
+    "num_episodes": 10_000,
+    "log_interval": 1000,
+    "state_bounds": state_bounds_lander
+}
+
