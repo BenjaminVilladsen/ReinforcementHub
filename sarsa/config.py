358649d8d0eaf680f5c43ea4f4455701730f2b5f
Benjamin Michael Villadsen <benmicvil@gmail.com>
2024-05-16 15:48:15 +0200

    Update config.py

--- sarsa/config.py
+++ sarsa/config.py
@@ -4,26 +4,31 @@ state_bounds_lander = [(-1.5, 1.5), (-1.5, 1.5), (-5, 5), (-5, 5), (-3.1415927, 
 state_bounds_car = [(-1.2, 0.6), (-0.07, 0.07)]  # Corrected bounds
 state_bounds_cartpole = [(-2.4, 2.4), (-float("inf"), float("inf")), (-0.2095, 0.2095), (-float("inf"), float("inf"))]
 
+
 settings_lander = {
     "alpha": 0.1,
     "gamma": 0.9,
-    "epsilon": 0.1,
+    "epsilon": 1,  # You might also consider an epsilon decay strategy here
+    "epsilon_decay": 0.99995,
+    "epsilon_min": 0.01,
     "num_bins": 20,
-    "num_episodes": 30000,
-    "log_interval": 100,
+    "num_episodes": 20_000,
+    "log_interval": 1_000,
     "state_bounds": state_bounds_lander,
     "convergence_threshold": 0.01,
     "success_threshold": 200
 }
-
 settings_car = {
     "alpha": 0.1,
     "gamma": 0.9,
-    "epsilon": 0.1,
+    "epsilon": 1,  # You might also consider an epsilon decay strategy here
+    "epsilon_decay": 0.99995,
+    "epsilon_min": 0.01,
     "num_bins": 20,
     "num_episodes": 10_000,
     "log_interval": 500,
     "state_bounds": state_bounds_car,
+    "convergence_threshold": 0.01,
     "success_threshold": -150
 }
 
